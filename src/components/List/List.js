90c5fe8462bda5507e20d1a45824557cec8407c3
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-09-29 21:24:15 +0200

    Use of props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import PropTypes from 'prop-types';
 
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <Hero />
+        <Hero titleText={this.props.title} imgSrc={this.props.picture} altSrc={this.props.pictureDescription} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column title='Animals' />
+          <Column title='Plants' />
+          <Column title='Minerals' />
+        </div>
       </section>
     )
   }
