ef372827a5bc0134142b728ab44de9fda024ae03
Joanna Stachowicz <joannastachowicz19@gmail.com>
2020-10-02 21:41:50 +0200

    Fixed bugs found by ESLint

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,8 +12,8 @@ const Hero = props => (
 
 Hero.propTypes = {
   titleText: PropTypes.node.isRequired,
-  imgSrc: PropTypes.node.isRequired,
-  altSrc: PropTypes.node.isRequired,
+  imgSrc: PropTypes.node,
+  altSrc: PropTypes.node,
 };
 
 export default Hero;
